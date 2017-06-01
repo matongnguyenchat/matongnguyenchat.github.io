d1082f3abf29d0ad59fde893c34961ec4263f6e9
Nong Nhat <nhat.nong@vietis.vn>
2017-06-01 16:50:36 +0700

    Home page style done

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,16 @@
 
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
+    $("#right-side-bar").stick_in_parent({
+        inner_scrolling: false,
+        offset_top: $('nav').height() + 30
+    }).on("sticky_kit:stick", function(e) {
+        console.log($('#right-side-bar').width());
+        $('#right-side-bar').parent().width($('#right-side-bar').parent().width() -1);
+    });
+
+    $('a[href="#"]').click(function (e) {
+        e.preventDefault();
+    });
 });
+
